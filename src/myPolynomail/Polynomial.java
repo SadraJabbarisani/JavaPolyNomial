5fe5576cc6ae125c2d64dd11a3cd16d596f7e4b5
Sadra <sadrajabbarisani@gmail.com>
2024-07-20 16:40:37 +0330

    edit Files a bit, Term class have negetive problem

--- src/myPolynomail/Polynomial.java
+++ src/myPolynomail/Polynomial.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package myPolynomail;
 
-/**
- *
- * @author Sadra
- */
+import java.util.Arrays;
+
 public class Polynomial {
+    Term[] term;
+    String[] termString;
+
+    public Polynomial() {
+    }
     
+    public Polynomial(String input) {
+        termString = input.split("[-+]");
+        term = new Term[0];
+        for(String t : termString){
+            term = Arrays.copyOf(term, term.length+1);
+            term[term.length-1] = new Term(t);
+        }
+    }
+    
+    public String toString(){
+        String outPut = "";
+        for(Term t: term){
+            outPut += t.toString();
+        }
+        return outPut;
+    }
 }
--- src/myPolynomail/Term.java
+++ src/myPolynomail/Term.java
@@ -1,13 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package myPolynomail;
 
-/**
- *
- * @author Sadra
- */
 public class Term {
+    int coeff, pow;
+
+    public Term(int coeff, int pow) {
+        this.coeff = coeff;
+        this.pow = pow;
+    }
     
+    public Term(String input){
+        String [] Component = input.split("[X^]");
+        coeff = Integer.parseInt(Component[0]);
+        pow = Integer.parseInt(Component[2]);
+    }
+
+    public int getCoeff() {
+        return coeff;
+    }
+
+    public void setCoeff(int coeff) {
+        this.coeff = coeff;
+    }
+
+    public int getPow() {
+        return pow;
+    }
+
+    public void setPow(int pow) {
+        this.pow = pow;
+    }
+    
+    public boolean isPosetive(){
+        if (coeff > 0)
+            return true;
+        else{
+            return false;
+        }
+    }
+    
+    public String toString(){
+        
+        if(isPosetive())
+            return "+" + Math.abs(coeff) + "X^" + pow;
+        else{
+            return "-" + Math.abs(coeff) + "X^" + pow;
+        }
+    }
+    
+    public int evaluate(int x){
+        return (int)Math.pow(x, pow) * coeff;
+    }
 }
